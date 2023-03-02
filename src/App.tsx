5033db3b85e813bddffdac01c3677ffb74fcbe85
mrmtsu <81202muramatsu@gmail.com>
2023-03-03 08:32:35 +0900

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import styles from "./style";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>Navbar</div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Hero</div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats Bussiness Billing CardDeal Testimonials Clients CTA Footer
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
